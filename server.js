6d94b53faf7324960ef44fc182c4df87775a94c4
xceto <claudio.troncoso91@gmail.com>
2021-10-03 14:28:06 -0300

    heroku fix

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors({
 })); 
 app.use('/', router);
 
-const portApp = process.env.APP_PORT || 5000;
+const portApp = process.env.PORT || 5000;
 
 console.log('>>>>>>', process.env)
 app.listen(portApp, () => {
